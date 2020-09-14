79b68498ec4decf04e515f08e15cc97a2317af48
Allan <allanl_95@hotmail.com>
2020-09-14 16:58:00 -0700

    javascript input validation

--- base-apparel-coming-soon-master/script.js
+++ base-apparel-coming-soon-master/script.js
@@ -1,5 +1,21 @@
 const button = document.querySelector('.submit-button');
 
 button.addEventListener('click', () => {
-	console.log('clicked');
-})
+	const inputValue = document.getElementById('input').value;
+	const input = document.getElementById('input');
+
+	if(validateEmail(inputValue)) {
+		console.log(`Submitting email address: ${inputValue}`);
+		input.classList.remove('error');
+	}
+	else {
+		input.classList.add('error');
+		const errorText = document.querySelector('.container-left');
+		errorText.innerHTML += '<br /><span class="error">Please provide a valid email</span>';
+	}
+});
+
+function validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
